3af163685ef77cfab76decc0f04a3592f13d4411
NadifRahman <nadif.rah@gmail.com>
2026-02-21 17:20:16 -0500

    Init the spring repo + docker stuff

---
+++ backend/src/test/java/com/secondserving/secondserving/SecondservingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondserving.secondserving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondservingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
